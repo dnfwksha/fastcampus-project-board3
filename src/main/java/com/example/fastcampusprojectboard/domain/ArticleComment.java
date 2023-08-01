262ff3435acdb235acb6eb40c28e043597c6018e
Juhwan <dnfwksha@gmail.com>
2023-08-02 05:58:00 +0900

    1

--- src/main/java/com/example/fastcampusprojectboard/domain/ArticleComment.java
+++ src/main/java/com/example/fastcampusprojectboard/domain/ArticleComment.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class ArticleComment {
@@ -12,7 +13,10 @@ public class ArticleComment {
     private Article article;    //  게시글 (ID)
     private String content;     //  본문
 
-
+    private LocalDateTime createdAt;    //  생성일시
+    private String createdBy;   //  생성자
+    private LocalDateTime modifiedAt;   //  수정일시
+    private String modifiedBy;  //  수정자
 
 
 }
